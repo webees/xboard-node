fc3c11f6500d897eb64afab9ba66e5dfc9bca752
xihaxz <157572955+xihaxz@users.noreply.github.com>
2024-04-02 23:46:51 +0800

    Update xrayr.sh

--- scripts/xrayr.sh
+++ scripts/xrayr.sh
@@ -21,7 +21,7 @@ Log:
   Level: none                              # 日志级别：none, error, warning, info, debug
   AccessPath:                              # 访问日志路径：/etc/XrayR/access.Log
   ErrorPath:                               # 错误日志路径：/etc/XrayR/error.log
-DnsConfigPath:                             # DNS配置路径：/etc/XrayR/dns.json    # DNS配置的路径，请参考 https://xtls.github.io/config/dns.html 获取帮助
+DnsConfigPath: /etc/XrayR/dns.json         # DNS配置路径：/etc/XrayR/dns.json    # DNS配置的路径，请参考 https://xtls.github.io/config/dns.html 获取帮助
 RouteConfigPath:                           # 路由配置路径：/etc/XrayR/route.json  # 路由配置的路径，请参考 https://xtls.github.io/config/routing.html 获取帮助
 InboundConfigPath:                         # 自定义入站配置路径：/etc/XrayR/custom_inbound.json  # 自定义入站配置的路径，请参考 https://xtls.github.io/config/inbound.html 获取帮助
 OutboundConfigPath:                        # 自定义出站配置路径：/etc/XrayR/custom_outbound.json  # 自定义出站配置的路径，请参考 https://xtls.github.io/config/outbound.html 获取帮助
@@ -49,8 +49,8 @@ Nodes:
       ListenIP: 0.0.0.0                    # 监听的IP地址
       SendIP: 0.0.0.0                      # 发送数据包的IP地址
       UpdatePeriodic: 8                    # 更新节点信息的时间间隔，单位：秒
-      EnableDNS: false                     # 是否使用自定义DNS配置，请确保正确设置dns.json
-      DNSType: AsIs                        # DNS策略：AsIs, UseIP, UseIPv4, UseIPv6
+      EnableDNS: true                      # 是否使用自定义DNS配置，请确保正确设置dns.json
+      DNSType: UseIPv4                     # DNS策略：AsIs, UseIP, UseIPv4, UseIPv6
       DisableUploadTraffic: false          # 禁用上传流量到面板
       DisableGetRule: false                # 禁用从面板获取规则
       DisableIVCheck: false                # 禁用Shadowsocks的反回复保护
